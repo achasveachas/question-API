44ab7bf0637e42b088b43bdf1f7faa6dc2f6f7d2
Yechiel Kalmenson <contact@yechiel.me>
2017-10-02 22:40:53 -0400

    add show action to questions controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -6,5 +6,15 @@ class Api::V1::QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find_by(id: params[:id])
+    if @question
+      render 'questions/show.json.jbuilder', question: @question
+    else
+      render json: {
+        errors: {
+          message: "Page not found"
+        }
+      }, status: 404
+    end
   end
 end
