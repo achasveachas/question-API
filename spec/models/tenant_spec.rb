cdd870ce6a2850d3b6f4e944cd01d7f22bf3c550
Yechiel Kalmenson <contact@yechiel.me>
2017-10-03 00:08:33 -0400

    add counter spec to tenant model specs

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe Tenant, type: :model do
     it "gets created with an API key" do
         expect(@tenant.api_key).not_to be_nil
     end
+
+    it "has a counter that defaults to 0" do
+        expect(@tenant.counter).to eq(0)
+    end
 end
