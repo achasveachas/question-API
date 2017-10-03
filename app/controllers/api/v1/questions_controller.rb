0caee21ceaaebe1a67bacaf8d801fb0488a732a9
Yechiel Kalmenson <contact@yechiel.me>
2017-10-02 23:47:27 -0400

    add logic to controllers to check for API key

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::QuestionsController < ApplicationController
+  before_action :authorize
 
   def index
     @questions = Question.where(private: false)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :authorize
+
+  private
+
+  def authorize
+    @tenant = Tenant.find_by(api_key: params[:headers][:Authorization]&.split(" ")&.last)
+    if !@tenant
+      render json: {
+        errors: ["Incorrect API Key"]
+      }, status: 401
+    end
+  end
 end
